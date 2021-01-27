0b732eafb74091630ae831151786b9e889d3fc61
kzamore <zamore@gmail.com>
2021-01-27 00:28:01 -0600

    change parameters

--- velocity-ops/main.tf
+++ velocity-ops/main.tf
@@ -2,17 +2,13 @@ module "platform" {
 	source = "./platform"
 	openstack_password = var.openstack_password
 	openstack_host = var.openstack_host
-	public_subnet_cidr = "104.4.58.176/29" 
-	public_subnet_gateway = "104.4.58.182"
-	public_subnet_start_ip = "104.4.58.177"
-	public_subnet_end_ip = "104.4.58.180"
-	int_subnet_cidr = "172.17.20.0/24"
-	int_subnet_gateway = "172.17.20.2"
-	int_subnet_start_ip = "172.17.20.10"
-	int_subnet_end_ip = "172.17.20.200"
-	lan_subnet_cidr = "172.17.0.0/24"
-	lan_subnet_gateway = "172.17.0.2"
-	lan_subnet_start_ip = "172.17.0.10"
-	lan_subnet_end_ip = "172.17.0.200"
+	public_subnet_cidr = var.public_subnet_cidr
+	public_subnet_gateway = var.public_subnet_gateway
+	public_subnet_start_ip = var.public_subnet_start_ip
+	public_subnet_end_ip = var.public_subnet_end_ip
+	shared_subnet_cidr = var.shared_subnet_cidr
+	shared_subnet_gateway = var.shared_subnet_gateway
+	shared_subnet_start_ip = var.shared_subnet_start_ip
+	shared_subnet_end_ip = var.shared_subnet_end_ip
 }
 
--- velocity-ops/variables.tf
+++ velocity-ops/variables.tf
@@ -4,5 +4,27 @@ variable "openstack_host" {
 variable "openstack_password" {
   type = string
 }
-
-
+variable "public_subnet_cidr" {
+  type = string
+}
+variable "public_subnet_gateway" {
+  type = string
+}
+variable "public_subnet_start_ip" {
+  type = string
+}
+variable "public_subnet_end_ip" {
+  type = string
+}
+variable "shared_subnet_cidr" {
+  type = string
+}
+variable "shared_subnet_gateway" {
+  type = string
+}
+variable "shared_subnet_start_ip" {
+  type = string
+}
+variable "shared_subnet_end_ip" {
+  type = string
+}
