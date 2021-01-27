f654d4ee17fbcb166cd0719e3241727e309534bf
kzamore <zamore@gmail.com>
2021-01-27 00:33:16 -0600

    latest names

--- velocity-ops/platform/providers.tf
+++ velocity-ops/platform/providers.tf
@@ -15,12 +15,3 @@ provider "openstack" {
   auth_url    = "http://${var.openstack_host}:5000/v3"
   region      = "RegionOne"
 }
-
-#provider "namecheap" {
-# username = "aaccell"
-#  api_user = "aaccell" # Same as username
-#  token = "e7479d38639e439ca48aefb97cd55f47"
-#  ip = "76.30.101.145"
-#  use_sandbox = false #
-#}
-
