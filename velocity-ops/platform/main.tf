b75572acf6fcdef07fc4e835e2b0a86cdb397ae4
kzamore <zamore@gmail.com>
2021-07-15 00:54:17 -0500

    adds lb and idm

--- velocity-ops/platform/main.tf
+++ velocity-ops/platform/main.tf
@@ -17,6 +17,36 @@ resource "openstack_networking_floatingip_v2" "public_ip" {
   pool = "igw_wan"
 }
 
+resource "openstack_compute_instance_v2" "vlb" {
+  name            = "vlb.nodelogic.net"
+  image_id        = module.openstack.image_ubuntu2004_id
+  flavor_id       = module.openstack.flavor_us2_small_id
+  key_pair        = module.openstack.cloudkey
+  security_groups = ["default", "www","ssh"]
+
+  metadata = {
+    lb_hosts = join(", ", [
+				"${openstack_compute_instance_v2.rundeck.access_ip_v4}",
+				"${openstack_compute_instance_v2.idm.access_ip_v4}"
+			])
+  }
+  network {
+    name = module.openstack.network_shared_name
+  }
+}
+
+resource "openstack_compute_instance_v2" "idm" {
+  name            = "idm.nodelogic.net"
+  image_id        = module.openstack.image_ubuntu2004_id
+  flavor_id       = module.openstack.flavor_us2_large_id
+  key_pair        = module.openstack.cloudkey
+  security_groups = ["default", "www","ssh"]
+
+  network {
+    name = module.openstack.network_shared_name
+  }
+}
+
 resource "openstack_compute_instance_v2" "rundeck" {
   name            = "rundeck.nodelogic.net"
   image_id        = module.openstack.image_ubuntu2004_id
